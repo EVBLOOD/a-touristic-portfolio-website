a07e192597e505bdd923ed7a290bd951e0bb2a07
saad akllam <akllam.saad@ofppt-edu.ma>
2021-06-04 19:13:02 +0100

    new commit

--- Words.js
+++ Words.js
@@ -1,4 +1,3 @@
-// Wrap every letter in a span
 var textWrapper = document.querySelector('.ml11 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
